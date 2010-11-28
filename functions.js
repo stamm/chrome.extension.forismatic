b7ef29f7c660c899688e86a05e176eea86219824
Rustam Zagirov <rustam@zagirov.name>
2010-11-29 00:25:29 +0300

    Change responce from xml to json

--- functions.js
+++ functions.js
@@ -1,25 +1,17 @@
-function subTag(txt, tag)
-{
-	openTag = '<' + tag + '>';
-	return txt.substring(
-		txt.indexOf(openTag) + openTag.length,
-		txt.indexOf('</'+tag+'>')
-	);
-}
-
 function main()
 {
 	req = new XMLHttpRequest();
 	req.onload = function () {
 		var doc = req.responseText;
 		if (doc) {
-			author = subTag(doc, 'quoteAuthor');
-			text = subTag(doc, 'quoteText');
+			var json = eval('(' + doc + ')');
+			author = json.quoteAuthor;
+			text = json.quoteText;
 			console.log(author + ' ' + text);
 			showNotification(author, text);
 		}
 	};
-	req.open("GET", "http://api.forismatic.com/api/1.0/?method=getQuote&format=xml&key=" + Math.random()*1000000 + "&lang=" + localStorage['lang'], true);
+	req.open("GET", "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&key=" + Math.random()*1000000 + "&lang=" + localStorage['lang'], true);
 	req.send(null);
 }
 
