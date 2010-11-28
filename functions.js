67806d10bcaf2c2f63566ad48cbf83c699233745
Rustam Zagirov <rustam@zagirov.name>
2010-11-28 22:55:39 +0300

    First initial

---
+++ functions.js
@@ -0,0 +1,48 @@
+function subTag(txt, tag)
+{
+	openTag = '<' + tag + '>';
+	return txt.substring(
+		txt.indexOf(openTag) + openTag.length,
+		txt.indexOf('</'+tag+'>')
+	);
+}
+
+function main()
+{
+	req = new XMLHttpRequest();
+	req.onload = function () {
+		var doc = req.responseText;
+		if (doc) {
+			author = subTag(doc, 'quoteAuthor');
+			text = subTag(doc, 'quoteText');
+			console.log(author + ' ' + text);
+			showNotification(author, text);
+		}
+	};
+	req.open("GET", "http://api.forismatic.com/api/1.0/?method=getQuote&format=xml&key=" + Math.random()*1000000 + "&lang=" + localStorage['lang'], true);
+	req.send(null);
+}
+
+function showNotification(title, text)
+{
+	var notification = webkitNotifications.createNotification(
+		'',
+		title,
+		text
+	);
+	notification.show();
+	window.setInterval(function() {
+		notification.cancel();
+	}, 10000);
+}
+
+
+function init() {
+	if ( ! localStorage['refresh'])
+		return;
+	clearInterval(interval)
+	var interval = window.setInterval(function() {
+		main();
+	}, localStorage['refresh'] * 60000);
+	console.log(localStorage['refresh'] * 60000);
+}
---
+++ zip.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+zip chrome-ext-forismatic.zip ./* -x ".git" -x "zip.sh"
